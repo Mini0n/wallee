c7758da36e78a60853eee89a53cd17d59ebc3d0c
mini0n <mini0n.000@gmail.com>
2019-07-12 13:50:09 -0500

    card validations 1st

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,12 @@
 class Card < ApplicationRecord
   # belongs_to :customer #to be done later
+  # validates_associated :customer
 
+  validates :name, :name_on_card, :number, :expiry, :debit, presence: true
+  validates :number, format: { with: /\d{16}/ }
+  validate :valid_expiration
+
+  def valid_expiration
+    expiry.is_a?(Date) && expiry > Date.today
+  end
 end
