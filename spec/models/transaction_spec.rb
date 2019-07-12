2716a92c9f720d827790184dd4c00338d0e5d616
mini0n <mini0n.000@gmail.com>
2019-07-12 17:30:28 -0500

    transaction model 1st test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Association tests
+
+  it { should belong_to(:wallet_origin).optional }
+  it { should belong_to(:wallet_destiny).optional }
+  it { should belong_to(:card_origin).optional }
+  it { should belong_to(:card_destiny).optional }
+
+  it { should validate_presence_of(:amount) }
+  it { should validate_presence_of(:percentage) }
+  it { should validate_presence_of(:fixed) }
+  it { should validate_numericality_of(:amount) }
+  it { should validate_numericality_of(:percentage) }
+  it { should validate_numericality_of(:fixed) }
 end
