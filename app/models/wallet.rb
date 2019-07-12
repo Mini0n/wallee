2462e30a13fe7e092a38d2b45aa7f0a8fb7ac1f2
mini0n <mini0n.000@gmail.com>
2019-07-12 13:56:26 -0500

    wallet validations 1st

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,2 +1,8 @@
 class Wallet < ApplicationRecord
+  # belongs_to :customer #to be done later
+  # has_many :transaction
+
+  # validates_associated :customer
+  validates :balance, numericality: true, presence: true
+
 end
