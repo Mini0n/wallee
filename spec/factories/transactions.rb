34ad5002eb5d4251939681801cc160a5a0e2be97
mini0n <mini0n.000@gmail.com>
2019-07-12 23:01:30 -0500

    transaction factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :transaction do
-    wallet_origin { nil }
+    wallet_origin { nil } # factory: :wallet
     wallet_destiny { nil }
     card_origin { nil }
     card_destiny { nil }
-    amount { "9.99" }
-    percentage { "9.99" }
-    fixed { "9.99" }
+    amount { rand(1.0...11.0).round(2)  }
+    percentage { rand(1.0...11.0).round(2)  }
+    fixed { rand(1.0...11.0).round(2)  }
   end
 end
