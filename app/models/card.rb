38b5020f7029928d90b425c8d6f0367e2221581c
mini0n <mini0n.000@gmail.com>
2019-07-12 20:10:02 -0500

    Credit card number validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,12 @@
 class Card < ApplicationRecord
+  include ActiveModel::Validations
   # belongs_to :customer #to be done later
   has_many :card_origin_transaction, class_name: 'Transaction', foreign_key: 'card_origin'
   has_many :card_destiny_transaction, class_name: 'Transaction', foreign_key: 'card_destiny'
 
   # validates_associated :customer
   validates :name, :name_on_card, :number, :expiry, presence: true
+  validates :number, credit_card_number: true
   validates :debit, inclusion: { in: [true, false] }
   validates :number, format: { with: /\d{16}/ }
   validate :valid_expiration
