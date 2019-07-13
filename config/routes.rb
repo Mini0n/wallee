4f03f7da45ef7bab84fe0e69acfc1f0bc178cc67
mini0n <mini0n.000@gmail.com>
2019-07-13 02:10:52 -0500

    routes 1st

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :wallets, only: %i[index show] do
+    resources :transactions, only: %i[index show]
+  end
+
+  resources :cards do
+    resources :transactions, only: %i[index show]
+  end
+
+  # namespace :admin do
+  #   resources :users, :wallets
+  #   resources :transactions, only: [:index, :show]
+  # end
 end
