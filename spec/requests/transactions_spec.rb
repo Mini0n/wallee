a0eb33d2412ae61b1b9f44f3e46a659ae9a25612
mini0n <mini0n.000@gmail.com>
2019-07-13 11:10:15 -0500

    test using one card

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -3,20 +3,79 @@
 require 'rails_helper'
 
 RSpec.describe 'Transactions API', type: :request do
+
+  let(:card_credit) { create(:card, debit: false) }
+  let(:card_debit) { create(:card, debit: true) }
+
   let!(:transactions) { create_list(:transaction, 10) }
-  let(:transaction_id) { transactions.first.id }
+
   let(:wallet_id) { transactions.first.wallet_origin_id }
 
-  describe 'GET /transactions' do
-    before { get "/wallets/#{wallet_id}/transactions" }
+  let!(:transactions_to_card) {
+    create_list(
+      :transaction, 10,
+      wallet_origin_id: wallet_id,
+      wallet_destiny_id: nil,
+      card_destiny: card_debit
+    )
+  }
+  let!(:transactions_from_card) {
+    create_list(
+      :transaction, 10,
+      wallet_origin_id: nil,
+      wallet_destiny_id: wallet_id,
+      card_origin: card_credit
+    )
+  }
 
-    it 'returns transactions' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq 10
+  let(:transaction_id) { transactions.first.id }
+  let(:transaction_from_card_id) { transactions_from_card.first.id }
+  let(:transaction_to_card_id) { transactions_to_card.first.id }
+
+  describe 'GET /transactions' do
+    context 'wallet to wallet' do
+      before { get "/wallets/#{wallet_id}/transactions" }
+
+      it 'returns transactions' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq 30
+        expect(json.first['wallet_origin_id']).to be_truthy
+        expect(json.first['wallet_destiny_id']).to be_truthy
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status 200
+      end
     end
 
-    it 'returns status 200' do
-      expect(response).to have_http_status 200
+    context 'wallet to card' do
+      before { get "/wallets/#{wallet_id}/transactions" }
+
+      it 'returns transactions' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq 30
+        expect(json[11]['wallet_origin_id']).to be_truthy
+        expect(json[11]['card_destiny_id']).to be_truthy
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status 200
+      end
+    end
+
+    context 'card to wallet' do
+      before { get "/wallets/#{wallet_id}/transactions" }
+
+      it 'returns transactions' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq 30
+        expect(json[23]['card_origin_id']).to be_truthy
+        expect(json[23]['wallet_destiny_id']).to be_truthy
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status 200
+      end
     end
   end
 
@@ -44,6 +103,12 @@ RSpec.describe 'Transactions API', type: :request do
       it 'returns error message' do
         expect(response.body).to include "Couldn't find Transaction"
       end
+    end
+  end
+
+  describe 'POST /transactions' do
+    let(:valid_amounts) do
+      { amount: 33.0, percentage: 2.0, fixed: 3.0 }
     end
   end
 end
