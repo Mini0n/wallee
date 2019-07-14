75bccd769fc6f36dc87121b8611332642e208643
mini0n <mini0n.000@gmail.com>
2019-07-13 20:00:34 -0500

    authentication errors handling

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,9 +1,29 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
+
+
   included do
+    # Define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+
+    rescue_from ExceptionHandler::AuthError, with: :bad_token
+    rescue_from ExceptionHandler::MissingToken, with: :bad_token
+    rescue_from ExceptionHandler::InvalidToken, with: :bad_token
+
     rescue_from ActiveRecord::RecordNotFound do |er|
       json_response({ error: er.message.slice(0, er.message.index(' with'))}, :not_found)
     end
+  end
+
+  private
+
+  def bad_token(e)
+    json_response({ message: e.message }, :unauthorized)
   end
 end
