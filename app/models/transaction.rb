a5b0edacee9c03ef2236bd7b7943f7aec170f3d1
mini0n <mini0n.000@gmail.com>
2019-07-15 16:21:09 -0500

    updated transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,14 +2,14 @@
 
 class Transaction < ApplicationRecord
   belongs_to :wallet_origin, class_name: 'Wallet', optional: true
-
   belongs_to :wallet_destiny, class_name: 'Wallet', optional: true
 
   belongs_to :card_origin, class_name: 'Card', optional: true
   belongs_to :card_destiny, class_name: 'Card', optional: true
 
-  validates :amount, :percentage, :fixed,
-            numericality: { greater_than_or_equal_to: 0 }, presence: true
+  validates :amount,  numericality: { greater_than_or_equal_to: 0 }, presence: true
+  validates :percentage, :fixed, numericality: true
+
   validate :valid_operation
 
   # Valid operations:
@@ -17,7 +17,8 @@ class Transaction < ApplicationRecord
   # wallet_origin -> card_destiny
   # card_origin -> wallet_destiny (wallet of user)
   def valid_operation
-    if (wallet_origin && wallet_destiny || wallet_origin && card_destiny && card_destiny.debit ||
+    if (wallet_origin && wallet_destiny ||
+        wallet_origin && card_destiny && card_destiny.debit ||
         card_origin && wallet_destiny).nil?
       errors.add(:amount, 'invalid operation. Missing wallet(s) and/or card(s)')
     end
