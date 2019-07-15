a5f8f8455b3e3895c5e1eb5e4ffc2cd8e823167d
mini0n <mini0n.000@gmail.com>
2019-07-15 00:08:27 -0500

    Authorize Request and tests 1st

---
+++ spec/auth/authorize_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe AuthorizeRequest do
+  let(:user) { create(:user) }
+  let(:header){ { 'Auth' => token_generator(user.id) } }
+
+  subject(:bad_request){ AuthorizeRequest.new({}) }
+  subject(:yes_request){ AuthorizeRequest.new(header) }
+
+
+  describe '#call' do
+    context 'when valid request' do
+    end
+
+    context 'when invalid request' do
+    end
+  end
+end
