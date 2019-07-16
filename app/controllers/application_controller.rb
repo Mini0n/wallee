4fbbfd310d7b1f49e0f084249874bf23e3093e10
mini0n <mini0n.000@gmail.com>
2019-07-15 19:11:56 -0500

    admin_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,20 @@ class ApplicationController < ActionController::API
   include ExceptionHandler
 
   before_action :authorize_request
-  attr_reader :current_user
+  attr_reader :current_user, :admin_user
 
   private
 
   def authorize_request
     @current_user = (AuthorizeRequest.new(request.headers).call)[:user]
+  end
+
+  def admin_user
+    @admin_user ||= User.find(0)
+  rescue ActiveRecord::RecordNotFound => err
+    raise(
+      ExceptionHandler::NoAdminUser,
+      (Message.no_admin)
+    )
   end
 end
