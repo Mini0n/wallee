3b936b6df4fcc14dfb683d884e9e9cebdaba0d7d
mini0n <mini0n.000@gmail.com>
2019-07-15 19:12:15 -0500

    NoAdminUser error handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,6 +7,7 @@ module ExceptionHandler
   class AuthError < StandardError; end
   class MissingToken < StandardError; end
   class InvalidToken < StandardError; end
+  class NoAdminUser < StandardError; end
 
   included do
     # Define custom handlers
@@ -15,6 +16,7 @@ module ExceptionHandler
     rescue_from ExceptionHandler::AuthError, with: :bad_token
     rescue_from ExceptionHandler::MissingToken, with: :bad_token
     rescue_from ExceptionHandler::InvalidToken, with: :bad_token
+    rescue_from ExceptionHandler::NoAdminUser, with: 	:precondition_failed
 
     rescue_from ActiveRecord::RecordNotFound do |er|
       json_response({ error: er.message.slice(0, er.message.index(' with')) }, :not_found)
