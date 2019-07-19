c9750ef54f16413249112aeb1d261ee51c1b632a
mini0n <mini0n.000@gmail.com>
2019-07-19 10:59:20 -0500

    uniqueness validation for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :cards
 
   validates_presence_of :name, :email, :password_digest
+  validates :name, uniqueness: { case_sensitive: false, message: 'username already taken' }
+  validates :email, uniqueness: { case_sensitive: false, message: 'email already taken' }
 end
